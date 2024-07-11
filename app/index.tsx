21a6b58ccf29d742b16a6104134a515fe4e0d769
aboveStars <yunuskorkmazis384@outlook.com>
2024-07-11 23:53:45 +0300

    Enabled AppCheck.

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,40 @@
 import { useAuth } from "@/providers/AuthProvider";
 import { Redirect } from "expo-router";
-import React from "react";
+import React, { useEffect } from "react";
 import { ActivityIndicator, SafeAreaView } from "react-native";
+import appCheck from "@react-native-firebase/app-check";
 
 const index = () => {
   const authStatus = useAuth();
+
+  useEffect(() => {
+    handleConnectAppCheckServers();
+  }, []);
+
+  const handleConnectAppCheckServers = async () => {
+    try {
+      const provider = appCheck().newReactNativeFirebaseAppCheckProvider();
+
+      provider.configure({
+        apple: {
+          provider: "debug",
+          debugToken: process.env.EXPO_PUBLIC_DEBUG_TOKEN,
+        },
+      });
+
+      await appCheck().initializeAppCheck({
+        provider: provider,
+        isTokenAutoRefreshEnabled: true,
+      });
+
+      const { token } = await appCheck().getToken();
+    } catch (error) {
+      console.error(
+        "Error on connecting and creating app check token: ",
+        error
+      );
+    }
+  };
 
   if (authStatus === "loading")
     return (
@@ -20,7 +50,8 @@ const index = () => {
     );
 
   if (authStatus === "unauthenticated") return <Redirect href="/auth" />;
-  if (authStatus === "authenticated") return <Redirect href="/home/profile/[username]" />;
+  if (authStatus === "authenticated")
+    return <Redirect href="/home/profile/[username]" />;
 };
 
 export default index;
