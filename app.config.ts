168b05902c6c3d3522d7fbfb938e1476ce11e6b3
aboveStars <yunuskorkmazis384@outlook.com>
2024-07-27 18:30:26 +0300

    Minor changes have been made.

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Apidon-Dev",
   slug: "apidon-app-dev",
-  version: "1.0.5",
+  version: "1.0.6",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "apidon",
