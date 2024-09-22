50b91a52ef4985177d3fc5409415d9137cad7d60
aboveStars <yunuskorkmazis384@outlook.com>
2024-09-22 16:17:13 +0300

    Minor changes.

--- components/TabBar/TabBar.tsx
+++ components/TabBar/TabBar.tsx
@@ -8,6 +8,8 @@ import { useSetAtom } from "jotai";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useNotification } from "@/providers/NotificationProvider";
 
+import { BlurView } from "expo-blur";
+
 export default function MyTabBar({ state, navigation }: BottomTabBarProps) {
   const { bottom } = useSafeAreaInsets();
 
@@ -31,81 +33,85 @@ export default function MyTabBar({ state, navigation }: BottomTabBarProps) {
     <View
       style={{
         alignSelf: "center",
-
         bottom: bottom || 20,
-
         position: "absolute",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-
-        backgroundColor: "#343434",
         borderRadius: 35,
+        overflow: "hidden",
       }}
     >
-      <Animated.View
+      <BlurView
+        tint="dark"
+        intensity={75}
         style={{
-          position: "absolute",
-          width: buttonWidth,
-          height: buttonHeight,
-          transform: [{ translateX: animatedTranslateXValue }],
-          justifyContent: "center",
+          flexDirection: "row",
           alignItems: "center",
+          justifyContent: "space-between",
         }}
       >
-        <View
+        <Animated.View
           style={{
-            backgroundColor: "white",
-            height: "100%",
-            width: "100%",
-            borderRadius: 25,
+            position: "absolute",
+            width: buttonWidth,
+            height: buttonHeight,
+            transform: [{ translateX: animatedTranslateXValue }],
+            justifyContent: "center",
+            alignItems: "center",
           }}
-        />
-      </Animated.View>
-      {state.routes.map((route, index) => {
-        const isFocused = state.index === index;
-
-        const onPress = () => {
-          const event = navigation.emit({
-            type: "tabPress",
-            target: route.key,
-            canPreventDefault: true,
-          });
-
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name, route.params);
-          }
-
-          // Home Button Refreshing Logic
-          if (route.name === "feed" && isFocused) {
-            setHomeScreenParameters({
-              isHomeButtonPressed: true,
-            });
-          }
-        };
-
-        const onLongPress = () => {
-          navigation.emit({
-            type: "tabLongPress",
-            target: route.key,
-          });
-        };
-
-        return (
-          <TabBarButton
-            key={route.key}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            isFocused={isFocused}
-            routeName={route.name}
-            color={isFocused ? "black" : "white"}
-            buttonSize={24}
-            haveUnReadNotifications={
-              route.name === "notifications" ? haveUnread : undefined
-            }
+        >
+          <View
+            style={{
+              backgroundColor: "white",
+              height: "100%",
+              width: "100%",
+              borderRadius: 25,
+            }}
           />
-        );
-      })}
+        </Animated.View>
+        {state.routes.map((route, index) => {
+          const isFocused = state.index === index;
+
+          const onPress = () => {
+            const event = navigation.emit({
+              type: "tabPress",
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name, route.params);
+            }
+
+            // Home Button Refreshing Logic
+            if (route.name === "feed" && isFocused) {
+              setHomeScreenParameters({
+                isHomeButtonPressed: true,
+              });
+            }
+          };
+
+          const onLongPress = () => {
+            navigation.emit({
+              type: "tabLongPress",
+              target: route.key,
+            });
+          };
+
+          return (
+            <TabBarButton
+              key={route.key}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              isFocused={isFocused}
+              routeName={route.name}
+              color={isFocused ? "black" : "white"}
+              buttonSize={24}
+              haveUnReadNotifications={
+                route.name === "notifications" ? haveUnread : undefined
+              }
+            />
+          );
+        })}
+      </BlurView>
     </View>
   );
 }
