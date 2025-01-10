c5625d900f47353a6415653722e119ae03460630
aboveStars <yunuskorkmazis384@outlook.com>
2025-01-10 15:39:22 +0300

    Minor changes.

--- components/Post/PostSkeleon.tsx
+++ components/Post/PostSkeleon.tsx
@@ -9,12 +9,21 @@ const PostSkeleton = ({ height }: { height?: number }) => {
         height: height || 630,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "rgba(255,255,255,0.1)",
-        borderRadius: 25,
-        paddingHorizontal : 5,
+        paddingHorizontal: 5,
       }}
     >
-      <ActivityIndicator color="white" />
+      <View
+        style={{
+          width: "100%",
+          height: height || 630,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(255,255,255,0.1)",
+          borderRadius: 25,
+        }}
+      >
+        <ActivityIndicator color="white" />
+      </View>
     </View>
   );
 };
